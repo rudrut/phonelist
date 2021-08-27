668189fb4538c955e43bdc8080063bae63a7dcd0
Rudi Rutanen <rudi.rutanen@hotmail.com>
2021-08-27 14:14:21 +0300

    Final version for phonelist

--- views/products/_form.php
+++ views/products/_form.php
@@ -1,6 +1,6 @@
-<?php if (!empty($errors)) : ?>
+<?php if (!empty($errors)): ?>
     <div class="alert alert-danger">
-        <?php foreach ($errors as $error) : ?>
+        <?php foreach ($errors as $error): ?>
             <div><?php echo $error ?></div>
         <?php endforeach; ?>
     </div>
@@ -11,21 +11,20 @@
         <img src="/<?php echo $product['image'] ?>" class="product-img-view">
     <?php endif; ?>
     <div class="form-group">
-        <label>Product Image</label>
-        <br>
+        <label>Product Image</label><br>
         <input type="file" name="image">
     </div>
     <div class="form-group">
-        <label>Product Title</label>
-        <input type="text" name="title" class="form-control" value="<?php echo $title ?>">
+        <label>Product title</label>
+        <input type="text" name="title" class="form-control" value="<?php echo $product['title'] ?>">
     </div>
     <div class="form-group">
-        <label>Product Description</label>
-        <textarea class="form-control" name="description"><?php echo $description ?></textarea>
+        <label>Product description</label>
+        <textarea class="form-control" name="description"><?php echo $product['description'] ?></textarea>
     </div>
     <div class="form-group">
-        <label>Product Price</label>
-        <input type="number" step=".01" name="price" value="<?php echo $price ?>" class="form-control">
+        <label>Product price</label>
+        <input type="number" step=".01" name="price" class="form-control" value="<?php echo $product['price'] ?>">
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
